7e0d9a377c6cbf3d3fa8fd31970607a6a34b760d
Hans-Codes <63604037+Hans-Codes@users.noreply.github.com>
2024-07-12 12:02:12 +0700

    Update utils.py
    Some changes on Time..

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
+# utils.py
+
 import json
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
 
 def load_config():
     with open('config.json', 'r') as f:
@@ -6,5 +10,25 @@ def load_config():
     return config
 
 def get_user_location():
-    location = input("Enter the city/town/country you want to see updates for: ").strip()
+    location = input("Enter the city/town/country you want to see updates for: ")
     return location
+
+def get_timezone(location):
+    geolocator = Nominatim(user_agent="weather_app")
+    location = geolocator.geocode(location)
+    
+    if location:
+        tf = TimezoneFinder()
+        timezone_str = tf.timezone_at(lng=location.longitude, lat=location.latitude)
+        return timezone_str
+    else:
+        return None
+
+def is_valid_location(location, api_key):
+    geolocator = Nominatim(user_agent="weather_app")
+    location_data = geolocator.geocode(location, language='en')
+    
+    if location_data:
+        return True
+    else:
+        return False
