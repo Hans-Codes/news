865b584796e35366f63bf4de2fdd54378f1bcc66
Hans-Codes <63604037+Hans-Codes@users.noreply.github.com>
2024-07-12 12:01:09 +0700

    Update news.py
    Updated news.py

--- news.py
+++ news.py
@@ -1,22 +1,11 @@
 import requests
 
-def get_news(location, category, api_key):
-    url = f"https://newsapi.org/v2/top-headlines?q={location}&category={category}&apiKey={api_key}"
+def get_news(location, category, api_key, shown_news_titles):
+    url = f"https://newsapi.org/v2/everything?q={location}&category={category}&apiKey={api_key}&language=en"
     response = requests.get(url)
-    try:
-        response.raise_for_status()
-        data = response.json()
-        articles = data['articles']
-        if not articles:
-            return None
-        news = []
-        for article in articles:
-            news.append({
-                "title": article['title'],
-                "url": article['url']
-            })
+    news_data = response.json()
+    
+    if news_data.get('status') == 'ok' and news_data.get('articles'):
+        news = [article for article in news_data['articles'] if article['title'] not in shown_news_titles]
         return news
-    except requests.exceptions.HTTPError as http_err:
-        return f"HTTP error occurred: {http_err} - {response.text}"
-    except Exception as err:
-        return f"Other error occurred: {err}"
+    return []
